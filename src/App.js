f685c0bd51606b1b4216fd709b81ce84835efbbd
Kashyap <abhilash.kashyap@capgemini.com>
2023-01-31 15:01:23 +0530

    Modified according to the latest updates

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
-import React from 'react';
-import Body from './Components/Body';
-import { API_KEY } from './env';
+import React from "react";
+import Body from "./Components/Body";
+import { API_KEY } from "./env";
 
-class App extends React.Component{
-  constructor (props){
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       searchResult: [],
       pictureOfTheDay: {},
-      data: []
-    }
+      data: [],
+    };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetchPictureOfTheDay();
+  }
+
+  fetchPictureOfTheDay = () => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((result) => {
+            this.setState({
+              pictureOfTheDay: result,
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  fetchPictureOfTheDay = () =>{
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,{
-      method: 'GET'
+  searchPictures = (search) => {
+    fetch(`https://images-api.nasa.gov/search?q=${search}`, {
+      method: "GET",
     }).then((response) => {
       if (response.ok) {
         response.json().then((result) => {
           this.setState({
-            pictureOfTheDay: result
+            searchResult: result.collection.items,
           });
         });
       }
-    }).catch((err) => {
-        console.log(err);
     });
-  }
+  };
 
-  searchPictures = (search) =>{
-    fetch(`https://images-api.nasa.gov/search?q=${search}`, {
-      method: 'GET'
-    }).then((response) =>{
-        if(response.ok) {
-          response.json().then((result)=>{
-            this.setState({
-              searchResult: result.collection.items
-            })
-          });
-        }
-    })
-  }
-
-  render () {
+  render() {
     console.log(this.state.searchResult);
     return (
       <>
-      <Body data={this.state.data} pictureOfTheDay={this.state.pictureOfTheDay} searchResult={this.state.searchResult} searchPictures={this.searchPictures}/>
+        <Body
+          data={this.state.data}
+          pictureOfTheDay={this.state.pictureOfTheDay}
+          searchResult={this.state.searchResult}
+          searchPictures={this.searchPictures}
+        />
       </>
-    )
+    );
   }
 }
 export default App;
